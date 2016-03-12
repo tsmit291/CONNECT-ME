a1151a1e281696b961698c4e6d696c53ef6b7d2d
= <tsmit291@icloud.com>
2016-03-12 12:24:21 -0700

    I can log in with linked in auth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+require('dotenv').load();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,15 +13,17 @@ router.post('/auth/linkedin', function(req, res) {
   var accessTokenUrl = 'https://www.linkedin.com/uas/oauth2/accessToken';
   var peopleApiUrl = 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url)';
   var params = {
+    grant_type: 'authorization_code',
     code: req.body.code,
     client_id: req.body.clientId,
-    client_secret: config.LINKEDIN_SECRET,
-    redirect_uri: req.body.redirectUri,
-    grant_type: 'authorization_code'
+    client_secret: process.env.LINKEDIN_SECRET,
+    redirect_uri: req.body.redirectUri
   };
+  console.log(params);
 
   // Step 1. Exchange authorization code for access token.
-router.post(accessTokenUrl, { form: params, json: true }, function(err, response, body) {
+request.post(accessTokenUrl, { form: params, json: true }, function(err, response, body) {
+  console.log('this is me localizing the problem');
   if (response.statusCode !== 200) {
     return res.status(response.statusCode).send({ message: body.error_description });
   }
